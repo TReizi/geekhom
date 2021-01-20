573737f8bbf11c9fd2ce634a2e0b63929550c544
Artem Lutsenko <34832181+TReizi@users.noreply.github.com>
2021-01-20 19:34:40 +0200

    Upload homework #5

--- 1.py
+++ 1.py
@@ -1,6 +1,13 @@
-my_list = [(-1 + 0j), 1, 2.2, True, None, 'String', [3, 4],
-            (5,6, 6.5), {7:'seven', 8: 'eight'},{9, 10},
-            frozenset(), range(11), (b'twelve'), bytearray (b'thirteen'),
-            zip(*[(14,15), (16,17), ('a', 'b')]), TypeError]
-for i, item in enumerate(my_list, 1):
-    print(f"{i}){item} - {type(item)}")
+my_f = open('test.txt', 'w')
+line = input('Введите текст \n')
+while line:
+    my_f.writelines(line)
+    line = input('Введите текст \n')
+    if not line:
+        break
+
+my_f.close()
+my_f = open('test.txt', 'r')
+content = my_f.readlines()
+print(content)
+my_f.close()
